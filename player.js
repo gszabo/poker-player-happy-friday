e7353cd1f6ffdc03f3db182c7b8ee2591321b5a0
blacksonic <soos.gabor86@gmail.com>
2015-10-02 10:32:01 +0200

    all in

--- player.js
+++ player.js
@@ -1,11 +1,32 @@
 
 module.exports = {
 
-  VERSION: "iPlayer 1.0",
+  VERSION: "iPlayer 2.0",
 
   bet_request: function(game_state) {
     var currentPlayer = game_state.players[game_state.in_action];
-    if (currentPlayer.hole_cards[0].rank == currentPlayer.hole_cards[1].rank) {
+
+    var cards = [].concat(currentPlayer.hole_cards).concat(game_state.community_cards);
+
+    var result = cards.reduce(function (previousValue, currentValue) {
+      if (previousValue[currentValue.rank]) {
+        previousValue[currentValue.rank]++;
+      }
+      else {
+        previousValue[currentValue.rank] = 1;
+      }
+
+      return previousValue;
+    }, {});
+
+    var shouldAllIn = false;
+    Object.keys(result).forEach(function (key) {
+      if (result[key] > 1) {
+        shouldAllIn = 1;
+      }
+    });
+
+    if (shouldAllIn) {
       return 1000;
     }
     else {
