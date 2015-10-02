65de495a6d0f91e02db5407b3f312a1597bfbc35
blacksonic <soos.gabor86@gmail.com>
2015-10-02 10:21:48 +0200

    iPlayer

--- player.js
+++ player.js
@@ -1,10 +1,16 @@
 
 module.exports = {
 
-  VERSION: "Default JavaScript folding player",
+  VERSION: "iPlayer 1.0",
 
   bet_request: function(game_state) {
-    return 1000;
+    var currentPlayer = game_state.players[game_state.in_action];
+    if (currentPlayer.hole_cards[0].rank == currentPlayer.hole_cards[1].rank) {
+      return 1000;
+    }
+    else {
+      return 0;
+    }
   },
 
   showdown: function(game_state) {
